cfc33e1808016c931de210d46827fab6a9b4c07c
zhalka <35832267+zhalka@users.noreply.github.com>
2024-06-25 14:47:35 +0300

    Update clip_interrogator.py

--- clip_interrogator/clip_interrogator.py
+++ clip_interrogator/clip_interrogator.py
@@ -36,25 +36,25 @@ class Config:
 
     # blip settings
     caption_max_length: int = 32
-    caption_model_name: Optional[str] = 'blip-large' # use a key from CAPTION_MODELS or None
+    caption_model_name: Optional[str] = 'blip2-2.7b' # use a key from CAPTION_MODELS or None
     caption_offload: bool = False
 
     # clip settings
-    clip_model_name: str = 'ViT-L-14/openai'
+    clip_model_name: str = '"ViT-bigG-14/laion2b_s39b_b160k'
     clip_model_path: Optional[str] = None
     clip_offload: bool = False
 
     # interrogator settings
     cache_path: str = 'cache'   # path to store cached text embeddings
     download_cache: bool = True # when true, cached embeds are downloaded from huggingface
-    chunk_size: int = 2048      # batch size for CLIP, use smaller for lower VRAM
+    chunk_size: int = 1024      # batch size for CLIP, use smaller for lower VRAM
     data_path: str = os.path.join(os.path.dirname(__file__), 'data')
     device: str = ("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
-    flavor_intermediate_count: int = 2048
+    flavor_intermediate_count: int = 1024
     quiet: bool = False # when quiet progress bars are not shown
 
     def apply_low_vram_defaults(self):
-        self.caption_model_name = 'blip-base'
+        self.caption_model_name = 'blip2-2.7b'
         self.caption_offload = True
         self.clip_offload = True
         self.chunk_size = 1024
